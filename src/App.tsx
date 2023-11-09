cf9f4c7ca473b365d25303ae216cd1d7183dac93
kila <luka.kilasonia.1@btu.edu.ge>
2023-11-09 21:19:49 +0400

    add content to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
 import "./App.css";
 
 function App() {
+  const padsArr = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
+
   return (
     <>
-      <div>Hello</div>
+      <div id="drum-machine">
+        <div className="drums">
+          {padsArr.map((item) => {
+            return <div className="drum-pad">{item}</div>;
+          })}
+        </div>
+
+        <div className="controls">
+          <div className="power">
+            <p>Power</p>
+            <div className="select">
+              <div className="power-turn"></div>
+            </div>
+          </div>
+          <div id="display">chord</div>
+          <div className="volume-slider">
+            <input type="range" min={0} max={1} step={0.01} />
+          </div>
+          <div className="bank">
+            <p>Bank</p>
+            <div className="select">
+              <div className="power-turn"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
